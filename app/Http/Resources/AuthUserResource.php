f4bcc06b006bb51aad597b9cd6817ae54948c485
depak-cyber <54800398+depak-cyber@users.noreply.github.com>
2025-05-20 09:58:18 +1000

    added optional in email verified

--- app/Http/Resources/AuthUserResource.php
+++ app/Http/Resources/AuthUserResource.php
@@ -20,11 +20,7 @@ class AuthUserResource extends JsonResource
           'name'=>$this->name,
           'created_at'=>$this->created_at,
           'email'=>$this->email,
-          'email_verified_at' => ($this->email_verified_at)
-          ? $this->email_verified_at->format('Y-m-d H:i:s')
-          : null,
-
- 
+          'email_verified_at' => optional($this->email_verified_at)->format('Y-m-d H:i:s'),
 
           'permissions'=>$this->getAllPermissions()
           ->map(function($permission){
